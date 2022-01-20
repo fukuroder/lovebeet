b553db52a930022528880a5d810196dc5d8577b9
fukuroda <fukuroder@live.jp>
2022-01-20 23:04:30 +0900

    formatter

--- lovebeet.py
+++ lovebeet.py
@@ -1,26 +1,28 @@
 import cv2
 import numpy as np
 
+
 def draw(scale, width, div, grad1, color1, color2, color3, lines, angle, pad, grad2, color4, color5):
-    assert scale > 0 and scale%2 == 1
+    assert scale > 0 and scale % 2 == 1
     assert 0 < width
     assert 0 < div
     assert 0 <= grad1 <= 100
-    assert 0 <= color1[0] <=255 and 0 <= color1[1] <=255 and 0 <= color1[2] <=255 
-    assert 0 <= color2[0] <=255 and 0 <= color2[1] <=255 and 0 <= color2[2] <=255 
-    assert 0 <= color3[0] <=255 and 0 <= color3[1] <=255 and 0 <= color3[2] <=255 
+    assert 0 <= color1[0] <= 255 and 0 <= color1[1] <= 255 and 0 <= color1[2] <= 255
+    assert 0 <= color2[0] <= 255 and 0 <= color2[1] <= 255 and 0 <= color2[2] <= 255
+    assert 0 <= color3[0] <= 255 and 0 <= color3[1] <= 255 and 0 <= color3[2] <= 255
     assert 0 < lines
     assert 0 <= angle < 360
     assert 0 <= pad
     assert 0 <= grad2 <= 100
-    assert 0 <= color4[0] <=255 and 0 <= color4[1] <=255 and 0 <= color4[2] <=255 
-    assert 0 <= color5[0] <=255 and 0 <= color5[1] <=255 and 0 <= color5[2] <=255 
+    assert 0 <= color4[0] <= 255 and 0 <= color4[1] <= 255 and 0 <= color4[2] <= 255
+    assert 0 <= color5[0] <= 255 and 0 <= color5[1] <= 255 and 0 <= color5[2] <= 255
 
     x = __draw1(scale, width, div, grad1, color1, color2, color3)
     x = __draw2(x, lines)
     x = __draw3(x, angle)
     x = __draw4(x, scale, pad, grad2, color4, color5)
     return x
+
 
 def __draw1(scale, width, div, grad_s, color1, color2, color3):
     width = width*scale
@@ -35,28 +37,29 @@ def __draw1(scale, width, div, grad_s, color1, color2, color3):
 
     ones = np.ones((grad_s,), dtype=np.float32)
     grad = np.linspace(1, 0, width-grad_s, endpoint=False, dtype=np.float32)
-    grad = grad*grad*(3-2*grad) # 3rd order interpolation
+    grad = grad*grad*(3-2*grad)  # 3rd order interpolation
     grad = np.r_[ones, grad]
     grad = grad[None, :, None]
     x = green_back * (grad) + white_back * (1-grad)
-    
+
     pos = np.arange(width)
     mask2 = (pos // div)
-    mask1 = ((pos*div)%width)//div
+    mask1 = ((pos*div) % width)//div
 
-    mask2[mask2<scale]=scale
-    mask2[(mask2>mask2[-1]-scale) & (mask2 < mask2[-1]) ]= mask2[-1]-scale
+    mask2[mask2 < scale] = scale
+    mask2[(mask2 > mask2[-1]-scale) & (mask2 < mask2[-1])] = mask2[-1]-scale
 
-    mask = (mask1 <= mask2) 
-    
+    mask = (mask1 <= mask2)
+
     x[:, mask, :] = color3
     return x
+
 
 def __draw2(x, lines):
     width = x.shape[1]
 
     pos = np.arange(width)
-    mask = ((pos*lines) // width) % 2 ==0 
+    mask = ((pos*lines) // width) % 2 == 0
 
     # slow
     # mask = mask[:, None, None]
@@ -66,11 +69,12 @@ def __draw2(x, lines):
     # fast
     xx = np.zeros((width, width, 3), dtype=np.float32)
     xx[mask] = x
-    xx[~mask] = x[:,::-1]
+    xx[~mask] = x[:, ::-1]
     return xx
 
+
 def __draw3(x, angle):
-    # add alpha channel    
+    # add alpha channel
     x = cv2.cvtColor(x, cv2.COLOR_BGR2BGRA)
 
     # rotate coarsely
@@ -85,15 +89,17 @@ def __draw3(x, angle):
     elif angle < 360:
         angle -= 270
         x = cv2.rotate(x, cv2.ROTATE_90_CLOCKWISE)
-    
+
     # rotate finely
     if angle > 0:
         adjust = np.sqrt(2) * np.sin(angle/180*np.pi + np.pi/4)
         trans = cv2.getRotationMatrix2D((0, 0), angle, 1)
         width = x.shape[1]
-        trans[1,2] = width*np.sin(angle/180*np.pi)
-        x = cv2.warpAffine(x, trans, (int(adjust*width)+1, int(adjust*width)+1), flags=cv2.INTER_NEAREST)
+        trans[1, 2] = width*np.sin(angle/180*np.pi)
+        x = cv2.warpAffine(x, trans, (int(adjust*width)+1,
+                           int(adjust*width)+1), flags=cv2.INTER_NEAREST)
     return x
+
 
 def __draw4(x, scale, pad, grad_s, color3, color4):
     x_width = x.shape[1]
@@ -110,17 +116,19 @@ def __draw4(x, scale, pad, grad_s, color3, color4):
 
     ones = np.ones((grad_s,), dtype=np.float32)
     grad = np.linspace(1, 0, width-grad_s, endpoint=False, dtype=np.float32)
-    grad = grad*grad*(3-2*grad) # 3rd order interpolation
+    grad = grad*grad*(3-2*grad)  # 3rd order interpolation
     grad = np.r_[ones, grad]
 
     grad = grad[None, :, None]
     bk = bk1 * (grad) + bk2 * (1-grad)
 
-    bk[pad:pad+x_width, pad:pad+x_width] = x[:,:,:3] * x[:,:,3:4] + bk[pad:pad+x_width, pad:pad+x_width]*(1-x[:,:,3:4])
+    bk[pad:pad+x_width, pad:pad+x_width] = x[:, :, :3] * x[:, :, 3:4] + \
+        bk[pad:pad+x_width, pad:pad+x_width]*(1-x[:, :, 3:4])
 
     if scale > 1:
         #bk = cv2.GaussianBlur(bk, (scale,scale), 0, borderType=cv2.BORDER_REPLICATE)
         #bk = bk[(scale-1)//2::scale, (scale-1)//2::scale]
-        bk = cv2.resize(bk, (bk.shape[1]//scale, bk.shape[0]//scale), interpolation=cv2.INTER_AREA)
-    
+        bk = cv2.resize(
+            bk, (bk.shape[1]//scale, bk.shape[0]//scale), interpolation=cv2.INTER_AREA)
+
     return np.clip(bk, 0, 255).astype(np.uint8)
